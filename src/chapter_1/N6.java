26b54f77720377f33e8dbf0f6b2920a5c3f53393
kuknishvilidato <dato.kuknishvili.1@btu.edu.ge>
2024-03-24 17:54:46 +0400

    Chapter 1

---
+++ src/chapter_1/N6.java
@@ -0,0 +1,13 @@
+package chapter_1;
+
+public class N6 {
+    public static void main(String[] args) {
+
+        int x = 0;
+        for (int i = 1; i <=9 ; i++) {
+            x += i;
+        }
+        System.out.println(x);
+
+    }
+}
---
+++ src/chapter_1/N9.java
@@ -0,0 +1,13 @@
+package chapter_1;
+
+public class N9 {
+    public static void main(String[] args) {
+
+        double width = 4.5;
+        double height = 7.9;
+
+        System.out.printf("Area: %f\n", width*height);
+        System.out.printf("Perimeter: %f", 2*(width+height));
+
+    }
+}
